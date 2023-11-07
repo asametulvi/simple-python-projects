66c18e4bf1213191f4ac0a53899f8b0c62663617
ulvisamet <asametulvioutlook.com>
2023-11-07 18:28:37 +0300

    Initial Commit

--- TotalOfnNumber.py
+++ TotalOfnNumber.py
@@ -1,20 +1,21 @@
 from time import sleep
 
 while True:
-
-    number = eval(input("how many numbers do you want to add? - "))
-    result = 0
-
-    if (number == 1):
-        print("enter 1 number")
-    elif (number > 1):
-        print("enter {number} numbers".format(number=number))
-    
+    while True:
+        number = eval (input ("how many consecutive numbers do you want to sum? - "))
+        result = 0    
+        if (number <= 1):
+            print ("\nenter a number which is greater than 1 \n")
+            continue
+        else:
+            print("enter {number} numbers".format(number=number))
+            break
+            
     while number >= 1 :
         a=input("")
         number=number-1
         result = result + int(a)
-        
+            
     print("calculating total... \n")
     sleep(2)
     print("total of numbers is : {result} \n\n".format(result=result))
